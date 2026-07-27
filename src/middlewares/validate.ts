3a597defe9eed08c279f0619e8eff055d75af746
avi9984 <avinashkumar151199@gmail.com>
2026-07-27 18:50:55 +0530

    Added Jod Validation

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,18 @@
+import { NextFunction,Request,Response } from "express";
+import { ZodSchema } from "zod";
+import { badRequest } from "../utils/api-error.js";
+
+export const validate=(schema:ZodSchema)=>
+(req:Request,_res:Response,next:NextFunction)=>{
+
+    const result=schema.safeParse(req.body);
+
+    if(!result.success){
+        throw badRequest('Validation failed',result.error.issues);
+    }
+
+    // If the validation passes
+    req.body=result.data;
+
+    next(); // calling controller with the validation data
+}
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUser, getAllUsers, getUserById } from "../controllers/user.controllers.js";
+import { validate } from "../middlewares/validate.js";
+import { createUserSchema } from "../dtos/user.dto.js";
 
 export const userRouter: Router = Router();
 
 userRouter.get('/', getAllUsers);
 userRouter.get('/:id', getUserById);
-userRouter.post('/', createUser);
+userRouter.post('/', validate(createUserSchema), createUser);
