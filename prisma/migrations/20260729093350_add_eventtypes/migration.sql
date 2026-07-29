ea1e441907a6055173232562b3249a2da45cc3f6
avi9984 <avinashkumar151199@gmail.com>
2026-07-29 15:06:07 +0530

    Added new event type schema

---
+++ prisma/migrations/20260729093350_add_eventtypes/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "event_type" (
+    "id" SERIAL NOT NULL,
+    "hostId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "durationMinutus" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "locationType" TEXT NOT NULL DEFAULT 'Online',
+    "locationValue" TEXT,
+    "bufferBeforeMinutes" INTEGER NOT NULL DEFAULT 0,
+    "bufferAfterMinutes" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "event_type_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "event_type" ADD CONSTRAINT "event_type_hostId_fkey" FOREIGN KEY ("hostId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
