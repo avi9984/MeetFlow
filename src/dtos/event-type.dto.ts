ba07549fec2a7b92211950589a59be4ee88d33ea
avi9984 <avinashkumar151199@gmail.com>
2026-08-03 21:06:30 +0530

    added the slug in dto

--- src/dtos/event-type.dto.ts
+++ src/dtos/event-type.dto.ts
@@ -1,19 +1,20 @@
-import {z} from "zod";
+import { z } from "zod";
 
-export const createEventTypeSchema=z.object({
+export const createEventTypeSchema = z.object({
     title: z.string().min(1).max(200),
-    description:z.string().min(1000).optional(),
+    description: z.string().min(1000).optional(),
     durationMinutes: z.number().min(15).max(120).default(30),
-    isActive:z.boolean().default(true),
+    isActive: z.boolean().default(true),
 
-    locationType:z.enum(['online','in-persion']).default('online'),
-    locationValue:z.string().optional(),
-    bufferBeforeMinutes:z.number().min(0).max(120).default(0),
-    bufferAfterMinutes:z.number().min(0).max(120).default(0)
+    locationType: z.enum(['online', 'in-persion']).default('online'),
+    locationValue: z.string().optional(),
+    bufferBeforeMinutes: z.number().min(0).max(120).default(0),
+    bufferAfterMinutes: z.number().min(0).max(120).default(0),
+    slug: z.string().min(1).max(200).regex(/^[a-z0-9-]+$/, "Slug must only contain lowercase letters, numbers, and hyphens"),
 });
 
-export const UpdateEventTypeSchema=createEventTypeSchema.partial();
+export const UpdateEventTypeSchema = createEventTypeSchema.partial();
 
 
-export type CreateEventTypeDto=z.infer<typeof createEventTypeSchema>;
-export type UpdateEventTypeDto=z.infer<typeof UpdateEventTypeSchema>;
+export type CreateEventTypeDto = z.infer<typeof createEventTypeSchema>;
+export type UpdateEventTypeDto = z.infer<typeof UpdateEventTypeSchema>;
