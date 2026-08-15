6603209b31cabf6acb0a29c85779e270ccad1d1f
avi9984 <avinashkumar151199@gmail.com>
2026-08-15 17:46:40 +0530

    added the temporal env

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,7 @@ export const PORT = process.env.PORT || 3000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const NODE_DEV: string = process.env.NODE_DEV || 'development';
 export const SLOT_GENERATION_DAYS = Number(process.env.SLOT_GENERATION_DAYS || 14);
+
+export const TEMPORAL_ADDRESS = process.env.TEMPORAL_ADDRESS || 'localhost:7233';
+export const TEMPORAL_NAMESPACE = process.env.TEMPORAL_NAMESPACE || 'default';
+export const TEMPORAL_TASK_QUEUE = process.env.TEMPORAL_TASK_QUEUE || 'meetflow-task'
