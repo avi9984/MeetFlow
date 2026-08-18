0cc47da05988f98e8c0e14b3f99d250b9e52bd1c
avi9984 <avinashkumar151199@gmail.com>
2026-08-18 15:24:27 +0530

    Added query middelware to handle that query where we use multiple of the data

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,18 +1,28 @@
-import { NextFunction,Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ZodSchema } from "zod";
 import { badRequest } from "../utils/api-error.js";
 
-export const validate=(schema:ZodSchema)=>
-(req:Request,_res:Response,next:NextFunction)=>{
+export const validate = (schema: ZodSchema) =>
+    (req: Request, _res: Response, next: NextFunction) => {
 
-    const result=schema.safeParse(req.body);
+        const result = schema.safeParse(req.body);
 
-    if(!result.success){
-        throw badRequest('Validation failed',result.error.issues);
+        if (!result.success) {
+            throw badRequest('Validation failed', result.error.issues);
+        }
+
+        // If the validation passes
+        req.body = result.data;
+
+        next(); // calling controller with the validation data
     }
 
-    // If the validation passes
-    req.body=result.data;
+export const validateQuery = (schema: ZodSchema) => (req: Request, _res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.query);
 
-    next(); // calling controller with the validation data
+    if (!result.success) {
+        throw badRequest('Validation failed', result.error.issues);
+    }
+    req.query = result.data as Request['query'];
+    next();
 }
