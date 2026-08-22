f6a6f4431351b039f2d25be36e1a90fa0de4c61a
avi9984 <avinashkumar151199@gmail.com>
2026-08-22 14:40:26 +0530

    added the google setup

--- src/utils/google-setup.ts
+++ src/utils/google-setup.ts
@@ -1,3 +1,11 @@
 import { getSetUpAuthUrl } from "../services/google-calender.service.js";
+import { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } from "../config/env.js";
 
+console.log("=== Google Config Debug ===");
+console.log(`Client ID:    ${GOOGLE_CLIENT_ID}`);
+console.log(`Redirect URI: ${GOOGLE_REDIRECT_URI}`);
+console.log("===========================\n");
+
+console.log("Generated Auth URL:");
 console.log(getSetUpAuthUrl());
+
