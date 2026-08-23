f95e2dfe732fbc4d143cb7ac68dd90d07e131c52
avi9984 <avinashkumar151199@gmail.com>
2026-08-23 15:13:21 +0530

    remove some uknowing logs

--- src/utils/google-setup.ts
+++ src/utils/google-setup.ts
@@ -1,11 +1,4 @@
-import { getSetUpAuthUrl } from "../services/google-calender.service.js";
-import { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } from "../config/env.js";
+import { getSetupAuthUrl } from "../services/google-calender.service.js";
 
-console.log("=== Google Config Debug ===");
-console.log(`Client ID:    ${GOOGLE_CLIENT_ID}`);
-console.log(`Redirect URI: ${GOOGLE_REDIRECT_URI}`);
-console.log("===========================\n");
 
-console.log("Generated Auth URL:");
-console.log(getSetUpAuthUrl());
-
+console.log(getSetupAuthUrl());
