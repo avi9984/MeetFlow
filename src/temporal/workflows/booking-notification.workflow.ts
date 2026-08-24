feb5e63ba8745f1b93b0cff7186411ca78dc62ce
avi9984 <avinashkumar151199@gmail.com>
2026-08-24 20:39:46 +0530

    src/temporal/workflows/booking-notification.workflow.ts

--- src/temporal/workflows/booking-notification.workflow.ts
+++ src/temporal/workflows/booking-notification.workflow.ts
@@ -6,11 +6,12 @@ import type * as activities from "../activites/index.js";
 
 // Create the proxy activites
 
-const { sendBookingConfirmationEmailActivity } = proxyActivities<typeof activities>({
+const { sendBookingConfirmationEmailActivity, createGoogleCalendarEventActivity } = proxyActivities<typeof activities>({
     retry: { maximumAttempts: 3 },
     startToCloseTimeout: '10 minutes',
 })
 
 export async function sendBookingConfirmationEmailWorkflow(bookingId: number) {
+    await createGoogleCalendarEventActivity(bookingId);
     await sendBookingConfirmationEmailActivity(bookingId);
 }
