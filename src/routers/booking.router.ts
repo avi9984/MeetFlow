103f82e4d25e7bb248d627f419ccdc9fc50f1813
avi9984 <avinashkumar151199@gmail.com>
2026-08-25 14:54:19 +0530

    Added booking routes

--- src/routers/booking.router.ts
+++ src/routers/booking.router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { requireUserId } from "../middlewares/require-user-id.js";
 import { validate, validateQuery } from "../middlewares/validate.js";
-import { createBookingSchema, ListHostBookingsQuerySchema } from "../dtos/booking.dto.js";
+import { createBookingSchema, listHostBookingsQuerySchema } from "../dtos/booking.dto.js";
 import { list, create } from "../controllers/booking.controller.js";
 
 export const bookingRouter: Router = Router();
 
 bookingRouter.use(requireUserId);
 
-bookingRouter.get("/", validateQuery(ListHostBookingsQuerySchema), list);
+bookingRouter.get("/", validateQuery(listHostBookingsQuerySchema), list);
 bookingRouter.post("/", validate(createBookingSchema), create);
